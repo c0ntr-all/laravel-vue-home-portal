7e72c8abf9a09e8be387b7f117285d0f463511c0
c0ntr-all <serg.taturin@gmail.com>
2021-12-27 02:31:35 +0300

    Установлен и подключен element-ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+require('element-ui');
+require('element-ui/lib/theme-chalk/index.css');
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
