bdc01e0ab3a39f4950311f35f5f284b81861a42e
c0ntr-all <serg.taturin@gmail.com>
2022-01-05 21:19:31 +0300

    Install: Установлен element plus для Vue 3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,11 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import Element from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(store).use(router).mount('#portal')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(Element)
+  .mount('#portal')
