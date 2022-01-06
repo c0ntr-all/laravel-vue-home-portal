0c82cd8ad245eded71a606f2637c5475fd2e6d67
c0ntr-all <serg.taturin@gmail.com>
2022-01-07 00:43:22 +0300

    Add: Добавлен сайдбар из element plus

--- frontend/src/components/TheSidebar.vue
+++ frontend/src/components/TheSidebar.vue
@@ -1,17 +1,39 @@
 <template>
-  <div class="sidebar">
-
-  </div>
+  <el-row class="tac">
+    <el-col :span="24">
+      <el-menu default-active="2" class="el-menu-vertical">
+        <el-menu-item index="1">
+          <el-icon><icon-menu /></el-icon>
+          <span>Главная</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <el-icon><document /></el-icon>
+          <span>Финансы</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <el-icon><setting /></el-icon>
+          <span>Задачи</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
 </template>
 
-<style lang="scss" scoped>
-  .sidebar {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    width: 220px;
-    background: #fff;
-    box-shadow: 2px 0 10px rgba(0, 0, 0, .1);
-    z-index: 2;
+<script setup>
+  import {
+    Document,
+    Menu as IconMenu,
+    Setting,
+  } from '@element-plus/icons-vue'
+</script>
+
+<style scoped>
+  .el-menu-vertical:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-menu-item {
+    --el-menu-item-height: 44px;
+    --el-menu-item-font-size: 16px;
   }
 </style>
