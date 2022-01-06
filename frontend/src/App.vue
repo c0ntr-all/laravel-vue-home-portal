1a042c6a10794a7317e8253a726e684929c11566
c0ntr-all <serg.taturin@gmail.com>
2022-01-06 04:50:55 +0300

    Fix: Убран тестовый компонент

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,34 +1,20 @@
 <template>
-  <TheHeader msg="Welcome to Your Vue.js App"/>
+
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue'
 
 export default {
-  components: {TheHeader}
+  components: {}
 }
 </script>
 
 <style lang="scss">
-#portal {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+  #portal {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
-}
 </style>
