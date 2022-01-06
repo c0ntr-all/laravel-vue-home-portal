6604907e3d6141008ec6fb9ff46c3cdb22acd434
c0ntr-all <serg.taturin@gmail.com>
2022-01-06 22:54:54 +0300

    Add: Добавлены компоненты и роуты для них: Home, Finances, Tasks

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Finances from '../views/Finances.vue'
+import Tasks from '../views/Tasks.vue'
 
-
-
-export default router
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {path: '/home', component: Home, alias: '/'},
+    {path: '/finances', component: Finances, alias: '/finances'},
+    {path: '/tasks', component: Tasks, alias: '/tasks'}
+  ],
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active'
+})
