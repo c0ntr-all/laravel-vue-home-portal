ab90d87df43976047196d0214fba0b196e949ea1
c0ntr-all <serg.taturin@gmail.com>
2022-01-11 01:05:41 +0300

    Add: Добавлены роуты для Finances

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Finances\FinancesController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('finances')->group(function() {
+    Route::get('/', [FinancesController::class, 'index']);
+    Route::get('{finance}', [FinancesController::class, 'show']);
 });
