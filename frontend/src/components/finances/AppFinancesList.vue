c25f279e0f754e7f569030dc06508c4ce6481bd3
c0ntr-all <serg.taturin@gmail.com>
2022-01-13 00:48:28 +0300

    Update: Вёрстка карточки Finance

--- frontend/src/components/finances/AppFinancesList.vue
+++ frontend/src/components/finances/AppFinancesList.vue
@@ -1,11 +1,17 @@
 <template>
   <div v-if="data.length !== 0">
-    <div v-for="item in data" class="card">
-      <p>Date: {{ item.date }}</p>
-      <p>Description: {{ item.description }}</p>
-      <p>Shop: {{ item.shop }}</p>
-      <p>Summ: {{ item.summ }}</p>
-    </div>
+    <el-space direction="vertical">
+      <el-card v-for="item in data"  class="box-card" shadow="hover">
+        <div class="box-card__image">
+
+        </div>
+        <div class="box-card__content">
+          <p class="box-card__date">{{ item.doneAt }}</p>
+          <p class="box-card__comment">{{ item.comment }}</p>
+          <p class="box-card__summ"><b>{{ item.summ }}</b></p>
+        </div>
+      </el-card>
+    </el-space>
   </div>
   <div v-else class="card error">
     <h4>Данных нет</h4>
@@ -20,6 +26,34 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .box-card {
+    &__content {
+      p:not(:last-child) {
+        margin-bottom: 10px;
+      }
+    }
+    &__date {
+      line-height: 12px;
+      font-size: 80%;
+      color: #909399;
+    }
+  }
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .box-card {
+    width: 480px;
+  }
 </style>
