b7770b93e0fa895396e9deef67c90fc4fe024d81
c0ntr-all <serg.taturin@gmail.com>
2022-01-16 01:27:51 +0300

    Add: Подготовлены файлы для Task Lists

---
+++ database/factories/Tasks/TaskListFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories\Tasks;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskListFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
