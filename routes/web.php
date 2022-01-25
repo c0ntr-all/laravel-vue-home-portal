f14e4ea82ac99bacbc5c1924443569cd268accf6
c0ntr-all <serg.taturin@gmail.com>
2022-01-26 00:39:11 +0300

    Add: Контроллер для тестирования

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'test'])->name('test');
 
 Auth::routes();
 
