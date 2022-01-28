9c90cb8307eda07e85ff54321db57da28d92ed44
c0ntr-all <serg.taturin@gmail.com>
2022-01-28 22:38:13 +0300

    Update: Донастройка JWT на бэкенде

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 use App\Http\Controllers\Finances\FinancesController;
 use App\Http\Controllers\Tasks\TaskListController;
 
@@ -30,4 +32,11 @@ Route::prefix('tasks')->group(function() {
     Route::get('/', [TaskListController::class, 'index']);
     Route::get('{task}', [TaskListController::class, 'show']);
     Route::post('list/store', [TaskListController::class, 'store']);
+});
+
+Route::prefix('auth')->middleware('api')->group(function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
