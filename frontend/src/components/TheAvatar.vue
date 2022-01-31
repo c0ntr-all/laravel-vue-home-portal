56fbf97236ec9d5e5f1c9c3e5b07f98ea55e15f5
c0ntr-all <serg.taturin@gmail.com>
2022-02-01 00:39:44 +0300

    Feature: Сделан выход из системы

--- frontend/src/components/TheAvatar.vue
+++ frontend/src/components/TheAvatar.vue
@@ -15,10 +15,7 @@
             <el-icon><icon-menu /></el-icon>
             <span>Профиль</span>
           </el-menu-item>
-          <el-menu-item :index="'2'">
-            <el-icon><icon-menu /></el-icon>
-            <span>Выйти</span>
-          </el-menu-item>
+          <el-menu-item :index="'2'" @click="logout"><span>Выйти</span></el-menu-item>
         </el-menu>
       </div>
     </el-popover>
@@ -30,6 +27,21 @@
     Menu as IconMenu,
     ArrowDown
   } from '@element-plus/icons-vue'
+</script>
+<script>
+  import API from "../utils/api";
+
+  export default {
+    methods: {
+      logout() {
+        API.post('/api/auth/logout')
+          .then(response => {
+            localStorage.removeItem('access_token')
+            this.$router.push({name: 'login'})
+          })
+      }
+    }
+  }
 </script>
 
 <style scoped>
