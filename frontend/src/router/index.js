c45dc249a8a7e615365b3ef317e556b4d9254334
c0ntr-all <serg.taturin@gmail.com>
2022-01-31 23:24:32 +0300

    Update: Проверка на токен на этапе определения маршрута

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,14 +8,68 @@ import Login from '../views/auth/Login.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/profile', component: Profile, title: 'Профиль', alias: '/profile', menu: 0},
-    {path: '/login', component: Login, title: 'Авторизация', alias: '/login', menu: 0},
-    {path: '/home', component: Home, title: 'Главная', alias: '/', menu: 1},
-    {path: '/finances', component: Finances, title: 'Финансы', alias: '/finances', menu: 1},
-    {path: '/tasks', component: Tasks, title: 'Задачи', alias: '/tasks', menu: 1}
+    {
+      path: '/profile',
+      component: Profile,
+      title: 'Профиль',
+      name: 'profile',
+      alias: '/profile',
+      menu: 0
+    },
+    {
+      path: '/login',
+      component: Login,
+      title: 'Авторизация',
+      name: 'login',
+      alias: '/login',
+      menu: 0
+    },
+    {
+      path: '/home',
+      component: Home,
+      title: 'Главная',
+      name: 'home',
+      alias: '/',
+      menu: 1
+    },
+    {
+      path: '/finances',
+      component: Finances,
+      title: 'Финансы',
+      name: 'finances',
+      alias: '/finances',
+      menu: 1
+    },
+    {
+      path: '/tasks',
+      component: Tasks,
+      title: 'Задачи',
+      name: 'tasks',
+      alias: '/tasks',
+      menu: 1
+    }
   ],
   linkActiveClass: 'is-active',
   linkExactActiveClass: 'is-active'
+})
+
+router.beforeEach((to, from, next ) => {
+  const accessToken = localStorage.access_token
+
+  if(to.name !== 'login') {
+    if(!accessToken) {
+      return next({
+        name: 'login'
+      })
+    }
+  }
+  if(to.name === 'login' && accessToken) {
+    return next({
+      name: 'home'
+    })
+  }
+
+  next()
 })
 
 export default router
