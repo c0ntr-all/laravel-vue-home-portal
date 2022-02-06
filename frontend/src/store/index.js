350e8dfb2d6c5bfcb1576d79ca1df969d68b716a
c0ntr-all <serg.taturin@gmail.com>
2022-02-06 23:20:01 +0300

    Add: Добавлены actions авторизации и выхода в vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import axios from "axios";
+import API from "../utils/api";
 
 export default createStore({
   state: {
@@ -9,6 +11,25 @@ export default createStore({
   mutations: {
   },
   actions: {
+    async login({commit}, email, password) {
+      await axios.post('api/auth/login', {email: email, password: password})
+        .then(response => {
+          localStorage.setItem('access_token', response.data.access_token)
+          commit('auth_success', token, user)
+        }).catch(error => {
+          if(error.response.data.error) {
+            commit('auth_error')
+            localStorage.removeItem('access_token')
+          }
+        })
+    },
+    logout({commit}) {
+      API.post('/api/auth/logout')
+        .then(response => {
+          localStorage.removeItem('access_token')
+        })
+      commit('logout')
+    }
   },
   modules: {
   }
