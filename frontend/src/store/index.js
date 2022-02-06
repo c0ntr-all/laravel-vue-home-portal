9b0225237ea91cd1856b031ab4c0be73f396543e
c0ntr-all <serg.taturin@gmail.com>
2022-02-06 23:07:45 +0300

    Add: Добавлен state для vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,9 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    status: '',
+    token: localStorage.getItem('access_token') || '',
+    user: {}
   },
   mutations: {
   },
