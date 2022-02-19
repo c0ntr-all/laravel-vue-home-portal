162c7af27b4a65b8424a590b4bd0611e01b45d48
c0ntr-all <serg.taturin@gmail.com>
2022-02-19 23:27:36 +0300

    Add: Подготовка работы с модальным окном для добавления напоминаний

--- frontend/src/views/Reminds.vue
+++ frontend/src/views/Reminds.vue
@@ -1,13 +1,20 @@
 <template>
   <el-header><h2 class="page-header">Напоминания</h2></el-header>
   <el-main>
-    Reminds
+    <el-button
+      type="primary"
+      @click="showAddModal"
+      circle
+    >+</el-button>
   </el-main>
 </template>
-
 <script>
   export default {
-
+    methods: {
+      showAddModal() {
+        alert('test')
+      }
+    }
   }
 </script>
 
