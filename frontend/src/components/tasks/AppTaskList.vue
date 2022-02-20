b5d275cc904dd7635c76cf1dec1b6aeb46e20370
c0ntr-all <serg.taturin@gmail.com>
2022-02-20 23:42:52 +0300

    Update: Вёрстка элемента задачи в списке

--- frontend/src/components/tasks/AppTaskList.vue
+++ frontend/src/components/tasks/AppTaskList.vue
@@ -6,8 +6,8 @@
         <el-button class="button" type="text">Operation button</el-button>
       </div>
     </template>
-    <div class="text item" v-for="item in items">
-      {{ item.title }}
+    <div class="text task" v-for="item in items">
+      <a href="#" class="task__link" @click.prevent="openTask">{{ item.title }}</a>
     </div>
     <el-form @submit.prevent="createItem">
       <el-input placeholder="Введите заголовок!" v-model="newItemTitle" />
@@ -44,11 +44,14 @@
         //   console.log(data.result)
         // }
       },
+      openTask() {
+        alert('open task')
+      }
     }
   }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   .card-header {
     display: flex;
     justify-content: space-between;
@@ -59,8 +62,22 @@
     font-size: 14px;
   }
 
-  .item {
+  .task {
     margin-bottom: 18px;
+
+    &__link {
+      display: block;
+      padding: 8px;
+      text-decoration: none;
+      color: #000;
+      border-radius: 3px;
+      box-shadow: 0 1px 0 #091e4240;
+
+      &:hover {
+        background-color: #f4f5f7;
+        border-bottom-color: #091e4240;
+      }
+    }
   }
 
   .box-card {
