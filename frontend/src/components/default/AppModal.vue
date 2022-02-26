16b17c679743a6bd0825ce6e04ea3cda82abf1c7
c0ntr-all <serg.taturin@gmail.com>
2022-02-26 23:27:56 +0300

    Add: Подготовлен каркас модального окна

---
+++ frontend/src/components/default/AppModal.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="modal-mask">
+    <div class="modal-wrapper">
+      <div class="modal-container">
+
+        <div class="modal-header">
+          default header
+        </div>
+
+        <div class="modal-body">
+          default body
+        </div>
+
+        <div class="modal-footer">
+          default footer
+          <button class="modal-default-button" @click="$emit('close')">OK</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showModal: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: table;
+  }
+
+  .modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .modal-container {
+    width: 300px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    font-family: Helvetica, Arial, sans-serif;
+  }
+
+  .modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+  }
+
+  .modal-body {
+    margin: 20px 0;
+  }
+
+  .modal-default-button {
+    display: block;
+    margin-top: 1rem;
+  }
+
+  .modal-enter-active,
+  .modal-leave-active {
+    transition: opacity 0.5s ease;
+  }
+
+  .modal-enter-from,
+  .modal-leave-to {
+    opacity: 0;
+  }
+</style>
