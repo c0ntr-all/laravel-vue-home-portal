e05d49090e1249f273d02c60ce5dd9bc20545ae7
c0ntr-all <serg.taturin@gmail.com>
2022-03-07 23:37:22 +0300

    Fix: Убран setTimeout, который нужен был для тестов

--- frontend/src/views/Tasks.vue
+++ frontend/src/views/Tasks.vue
@@ -19,31 +19,28 @@
       }
     },
     methods: {
-      loadData() {
+      async loadData() {
         this.loading = true
-
-        setTimeout(async() => {
-          try {
-            const {data} = await API.get('api/auth/tasks')
-            if(!data) {
-              throw new Error('Нет данных!')
-            }
-            this.$store.state.lists = Object.keys(data.lists).map(key => {
-              return {
-                id: key,
-                ...data.lists[key]
-              }
-            })
-            this.loading = false
-          }catch(e) {
-            this.alert = {
-              type: 'danger',
-              title: 'Ошибка',
-              text: e.message
-            }
-            this.loading = false
+        try {
+          const {data} = await API.get('api/auth/tasks')
+          if(!data) {
+            throw new Error('Нет данных!')
           }
-        })
+          this.$store.state.lists = Object.keys(data.lists).map(key => {
+            return {
+              id: key,
+              ...data.lists[key]
+            }
+          })
+          this.loading = false
+        }catch(e) {
+          this.alert = {
+            type: 'danger',
+            title: 'Ошибка',
+            text: e.message
+          }
+          this.loading = false
+        }
       },
       async createList() {
         const response = await API.post('api/auth/tasks/create', {
