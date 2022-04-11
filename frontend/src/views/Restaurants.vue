022fa641b4221b9d8acd34257d86e0fee47512a8
c0ntr-all <serg.taturin@gmail.com>
2022-04-12 01:04:13 +0300

    Add: Добавлена страница Restaurants

---
+++ frontend/src/views/Restaurants.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-header><h2 class="page-header">Рестораны</h2></el-header>
+  <el-main>
+    Список посещенных ресторанов
+  </el-main>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
