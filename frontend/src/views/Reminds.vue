d576ba38061e12015bc5f99632b6df0d52d62702
c0ntr-all <serg.taturin@gmail.com>
2022-04-18 02:46:59 +0300

    Update: Вывод таблицы с Reminds

--- frontend/src/views/Reminds.vue
+++ frontend/src/views/Reminds.vue
@@ -1,6 +1,17 @@
 <template>
   <el-header><h2 class="page-header">Напоминания</h2></el-header>
-  <el-main>
+  <el-main v-loading="loading">
+    <el-table :data="this.$store.getters.reminds" style="width: 100%">
+      <el-table-column type="expand">
+        <template #default="props">
+          <p>Title: {{ props.row.title }}</p>
+          <p>Content: {{ props.row.content }}</p>
+          <p>Date: {{ props.row.datetime }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column label="Title" prop="title" />
+      <el-table-column label="Date" prop="datetime" />
+    </el-table>
     <el-button
       type="primary"
       @click="showAddModal"
@@ -12,6 +23,11 @@
   import API from "../utils/api";
 
   export default {
+    data() {
+      return {
+        loading: false,
+      }
+    },
     methods: {
       async loadData() {
         this.loading = true
@@ -38,7 +54,7 @@
         }
       },
       showAddModal() {
-        alert('test')
+        console.log(this.$store.getters.reminds)
       }
     },
     mounted() {
