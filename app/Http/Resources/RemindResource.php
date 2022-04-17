8850bccf1c50eedf05d74dc5d790a9a8062de5a8
c0ntr-all <serg.taturin@gmail.com>
2022-04-18 02:01:56 +0300

    Add: Добавлены ресурс и коллекция для Reminds

---
+++ app/Http/Resources/RemindResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RemindResource extends JsonResource
+{
+    public static $wrap = 'reminds';
+
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'datetime' => $this->datetime,
+            'isActive' => $this->is_active,
+            'createdAt' => $this->created_at,
+            'updatedAt' => $this->updated_at,
+        ];
+    }
+}
