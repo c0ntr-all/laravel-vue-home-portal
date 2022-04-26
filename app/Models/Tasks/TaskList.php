3c2e3a92e332f6528028a831a65fa7ac0afde0dc
c0ntr-all <serg.taturin@gmail.com>
2022-04-27 01:36:39 +0300

    Update: Вывод TaskLists с вложенными Tasks и Comments

--- app/Models/Tasks/TaskList.php
+++ app/Models/Tasks/TaskList.php
@@ -35,6 +35,6 @@ class TaskList extends Model
     public function getItems(): Collection
     {
         // Пока все берем
-        return TaskList::all();
+        return TaskList::with('tasks.comments')->get();
     }
 }
