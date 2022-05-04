c63f90e856e6256f93e6af69517bbae47361abab
c0ntr-all <serg.taturin@gmail.com>
2022-05-05 02:22:22 +0300

    Add: Requests для Comment

---
+++ app/Http/Requests/Comment/StoreRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Comment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'comment.content' => 'required|string|max:2048'
+        ];
+    }
+}
