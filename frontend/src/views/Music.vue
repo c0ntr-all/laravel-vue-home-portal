c01bf019000b6584a43d971bc4f51c7f5b3d9170
c0ntr-all <serg.taturin@gmail.com>
2022-05-09 00:01:16 +0300

    Update: Базовая верстка и тестовые данные

--- frontend/src/views/Music.vue
+++ frontend/src/views/Music.vue
@@ -1,6 +1,29 @@
 <template>
   <el-header><h2 class="page-header">Музыка</h2></el-header>
-  <el-main v-loading="loading">
+  <el-main>
+    <div class="tags">
+      <h3>Genres</h3>
+      <div class="tags-list">
+        <a href="" class="tag-link" @click.prevent="filterBands(tag.label)" v-for="tag in tags">
+          <el-tag
+            :key="tag.label"
+            :type="tag.type"
+            class="mx-1"
+            effect="dark"
+          >
+            {{ tag.label }}
+          </el-tag>
+        </a>
+      </div>
+    </div>
+    <div class="bands">
+      <h3>Artists</h3>
+      <div class="bands-list">
+        <div class="band-item" v-for="band in bands">
+          {{ band.name }}
+        </div>
+      </div>
+    </div>
   </el-main>
 </template>
 <script>
@@ -10,11 +33,33 @@
     data() {
       return {
         loading: false,
+        tags: [
+          {type: '', label: 'Metal'},
+          {type: 'success', label: 'Rock'},
+          {type: 'info', label: 'Break Beat'},
+          {type: 'danger', label: 'Industrial'},
+          {type: 'warning', label: 'Classic'},
+        ],
+        bands: [
+          {name: 'Fear Factory', tags: ['Metal','Rock','Industrial']},
+          {name: 'Metallica', tags: ['Metal','Rock']},
+          {name: 'Soulfly', tags: ['Metal','Rock']},
+          {name: 'Slipknot', tags: ['Metal','Rock']},
+          {name: 'Cannibal Corpse', tags: ['Metal','Rock']},
+          {name: 'Prodigy', tags: ['Break Beat','Industrial']},
+          {name: 'Bach', tags: ['Classic']}
+        ],
         model: {
         }
       }
     },
     methods: {
+      filterBands(tag) {
+        console.log(tag)
+      },
+      loadData() {
+
+      }
     },
     mounted() {
       this.loadData();
@@ -23,5 +68,16 @@
 </script>
 
 <style lang="scss" scoped>
-
+  h3 {
+    margin-top: 0;
+  }
+ .tags-list {
+   display: flex;
+   column-gap: 1rem;
+   margin-bottom: 1rem;
+ }
+  .tag-link {
+    display: block;
+    text-decoration: none;
+  }
 </style>
