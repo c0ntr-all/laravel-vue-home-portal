7565f15b57c0574d46f78dc7e2b6eccbfef14d76
c0ntr-all <serg.taturin@gmail.com>
2022-07-05 02:14:18 +0300

    Update: Скелет для Artist

--- frontend/src/views/music/Artist.vue
+++ frontend/src/views/music/Artist.vue
@@ -1,40 +1,60 @@
 <template>
   <div class="artist">
     <el-button type="primary" :icon="ArrowLeft" @click="this.$router.push('/music')">Вернуться назад</el-button>
-    <div class="artist-head">
-      <div class="artist-head__left">
-        <div class="artist-head__image">
-          <a :href="this.artist.image"><img :src="this.artist.image" alt=""></a>
-        </div>
-      </div>
-      <div class="artist-head__right">
-        <h2 class="artist-head__name">{{ artist.name }}</h2>
-        <div class="artist-head__description">
-          {{ artist.content }}
-        </div>
-        <div class="artist-head__tags">
-          <div class="tags-list">
-            <el-tag v-for="tag in artist.tags" class="mx-1">{{ tag }}</el-tag>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="artist-albums" v-if="this.artist.albums">
-      <h3>Альбомы</h3>
-      <el-space alignment="flex-start" wrap>
-        <el-card class="album-card" :body-style="{ padding: '0px' }" v-for="album in this.artist.albums" :key="album.id">
-          <div class="album-card__image">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="150" xmlns="http://www.w3.org/2000/svg" role="img" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="40%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
-          </div>
-          <div style="padding: 14px">
-            <router-link :to="'/music/artists/' + this.artist.id + '/albums/' + album.id"><span>{{ album.year }} - {{ album.title }}</span></router-link>
-            <div class="artist-card__footer">
-
+    <el-skeleton :loading="loading" animated>
+      <template #template>
+        <div class="artist-head">
+          <div class="artist-head__left">
+            <div class="artist-head__image">
+              <el-skeleton-item variant="image" style="width: 200px; height: 200px"/>
             </div>
           </div>
-        </el-card>
-      </el-space>
-    </div>
+          <div class="artist-head__right">
+            <el-skeleton-item variant="text" style="width: 500px; height: 50px; margin: 10px 0" />
+            <el-skeleton-item variant="text" style="width: 80%; margin: 10px 0" />
+            <el-skeleton-item variant="text" style="width: 90%; margin: 10px 0" />
+            <el-skeleton-item variant="text" style="width: 100%; margin: 10px 0" />
+            <el-skeleton-item variant="text" style="width: 60%; margin: 10px 0" />
+          </div>
+        </div>
+      </template>
+      <template #default>
+        <div class="artist-head">
+          <div class="artist-head__left">
+            <div class="artist-head__image">
+              <a :href="this.artist.image"><img :src="this.artist.image" alt=""></a>
+            </div>
+          </div>
+          <div class="artist-head__right">
+            <h2 class="artist-head__name">{{ artist.name }}</h2>
+            <div class="artist-head__description">
+              {{ artist.content }}
+            </div>
+            <div class="artist-head__tags">
+              <div class="tags-list">
+                <el-tag v-for="tag in artist.tags" class="mx-1">{{ tag }}</el-tag>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="artist-albums" v-if="this.artist.albums">
+          <h3>Альбомы</h3>
+          <el-space alignment="flex-start" wrap>
+            <el-card class="album-card" :body-style="{ padding: '0px' }" v-for="album in this.artist.albums" :key="album.id">
+              <div class="album-card__image">
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="150" xmlns="http://www.w3.org/2000/svg" role="img" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="40%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+              </div>
+              <div style="padding: 14px">
+                <router-link :to="'/music/artists/' + this.artist.id + '/albums/' + album.id"><span>{{ album.year }} - {{ album.title }}</span></router-link>
+                <div class="artist-card__footer">
+
+                </div>
+              </div>
+            </el-card>
+          </el-space>
+        </div>
+      </template>
+    </el-skeleton>
   </div>
 </template>
 <script setup>
