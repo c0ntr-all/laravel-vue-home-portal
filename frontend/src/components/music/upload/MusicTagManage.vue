b67fd5e8e1a6e84956ef88937dda6c6769e668c3
c0ntr-all <serg.taturin@gmail.com>
2022-07-12 01:36:04 +0300

    Add: Добавлена страница управления тегами

---
+++ frontend/src/components/music/upload/MusicTagManage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3>Управление тегами</h3>
+  <el-tabs tab-position="top">
+
+  </el-tabs>
+</template>
+<script setup>
+  import {
+    Plus
+  } from '@element-plus/icons-vue'
+</script>
+<script>
+  import API from "../../../utils/api";
+
+</script>
+
+<style lang="scss">
+
+</style>
--- frontend/src/views/music/MusicUpload.vue
+++ frontend/src/views/music/MusicUpload.vue
@@ -8,17 +8,22 @@
       <el-tab-pane label="Загрузка альбома">
         <music-upload-album></music-upload-album>
       </el-tab-pane>
+      <el-tab-pane label="Управление тегами">
+        <music-tag-manage></music-tag-manage>
+      </el-tab-pane>
     </el-tabs>
   </el-main>
 </template>
 <script>
   import MusicUploadArtist from "../../components/music/MusicUploadArtist";
   import MusicUploadAlbum from "../../components/music/MusicUploadAlbum";
+  import MusicTagManage from "../../components/music/upload/MusicTagManage";
 
   export default {
     components: {
       MusicUploadArtist,
-      MusicUploadAlbum
+      MusicUploadAlbum,
+      MusicTagManage
     }
   }
 </script>
