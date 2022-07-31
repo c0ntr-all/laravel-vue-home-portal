e86d398d70c6041592eb19dd14b1b3097bbe8ede
c0ntr-all <serg.taturin@gmail.com>
2022-07-31 16:34:42 +0300

    Add: Паттерн организации svg-иконок в vue приложении на примере с Track

---
+++ frontend/src/components/default/icons/IconBase.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 485 485"
+    :aria-labelledby="iconName"
+    role="presentation"
+    style="enable-background:new 0 0 485 485;"
+  >
+    <title :id="iconName" lang="en">{{ iconName }} icon </title>
+    <g :fill="iconColor"><slot /></g>
+  </svg>
+</template>
+<script>
+  export default {
+    props: {
+      iconName: {
+        type: String,
+        default: 'box'
+      },
+      width: {
+        type: [Number, String],
+        default: 18
+      },
+      height: {
+        type: [Number, String],
+        default: 18
+      },
+      iconColor: {
+        type: String,
+        default: 'currentColor'
+      }
+    }
+  }
+</script>
--- frontend/src/components/music/playing/MusicTrackCard.vue
+++ frontend/src/components/music/playing/MusicTrackCard.vue
@@ -1,27 +1,10 @@
 <template>
   <div class="track-card" @click="play(track)">
     <div class="track-card__play-icon" v-if="status === 'pause' || (status === 'play' && player.track.id !== track.id)">
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 485 485"
-           style="enable-background:new 0 0 485 485;"
-           xml:space="preserve"
-      >
-        <path d="M413.974,71.026C368.171,25.225,307.274,0,242.5,0S116.829,25.225,71.026,71.026C25.225,116.829,0,177.726,0,242.5
-          s25.225,125.671,71.026,171.474C116.829,459.775,177.726,485,242.5,485s125.671-25.225,171.474-71.026
-          C459.775,368.171,485,307.274,485,242.5S459.775,116.829,413.974,71.026z M242.5,455C125.327,455,30,359.673,30,242.5
-          S125.327,30,242.5,30S455,125.327,455,242.5S359.673,455,242.5,455z"/>
-        <polygon points="181.062,336.575 343.938,242.5 181.062,148.425 	"/>
-      </svg>
+      <icon-base icon-name="play"><icon-play /></icon-base>
     </div>
     <div class="track-card__play-icon" v-else>
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-           viewBox="0 0 485 485" style="enable-background:new 0 0 485 485;" xml:space="preserve">
-        <path d="M413.974,71.026C368.171,25.225,307.274,0,242.5,0S116.829,25.225,71.026,71.026C25.225,116.829,0,177.726,0,242.5
-          s25.225,125.671,71.026,171.474C116.829,459.775,177.726,485,242.5,485s125.671-25.225,171.474-71.026
-          C459.775,368.171,485,307.274,485,242.5S459.775,116.829,413.974,71.026z M242.5,455C125.327,455,30,359.673,30,242.5
-          S125.327,30,242.5,30S455,125.327,455,242.5S359.673,455,242.5,455z"/>
-        <rect x="172.5" y="140" width="55" height="205"/>
-        <rect x="257.5" y="140" width="55" height="205"/>
-      </svg>
+      <icon-base icon-name="stop"><icon-stop /></icon-base>
     </div>
     <div class="track-card__number">{{ track.number }}</div>
     <div class="track-card__name">{{ track.name }}</div>
@@ -30,6 +13,10 @@
 </template>
 <script>
   import {mapGetters, mapActions} from 'vuex'
+
+  import IconBase from "../../default/icons/IconBase";
+  import IconPlay from "../../default/icons/IconPlay";
+  import IconStop from "../../default/icons/IconStop";
 
   export default {
     props: {
@@ -40,7 +27,8 @@
     },
     computed: {
       ...mapGetters(['player','status'])
-    }
+    },
+    components: {IconBase, IconPlay, IconStop}
   }
 </script>
 <style lang="scss" scoped>
