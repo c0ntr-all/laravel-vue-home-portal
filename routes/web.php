eb1a21618bca9a5900022df859b951c46208140f
c0ntr-all <serg.taturin@gmail.com>
2022-11-27 23:48:00 +0300

    Test: Тестирование пакета для работы с id3v2 тегами аудио файлов

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Resources\Music\ArtistCollection;
 use App\Models\Music\Artist;
-use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
-use App\Models\Tasks\Task;
 use App\Models\Tasks\TaskList;
+use Illuminate\Contracts\Support\Jsonable;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,16 +26,34 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 /*Для тестирования*/
 Route::get('/test/{id}', [App\Http\Controllers\TagController::class, 'test'])->name('test');
 
-Route::get('/tasks/test2', function() {
-    $tasks = TaskList::find(1);
-    dd($tasks->getItems()->toArray());
+Route::get('/test2', function() {
+
+    $path = 'F:\Music\Metal\Cyber metal\Sybreed\2004 - Slave Design\01. Bioactive.mp3';
+
+    $mp3 = new Acekyd\LaravelMP3\LaravelMP3();
+    $artist = $mp3->getArtist($path); // Получить Исполнителя
+    $album = $mp3->getAlbum($path); // Получить Альбом
+    $year = $mp3->getYear($path); // Получить год
+    $genre = $mp3->getGenre($path); // Получить жанр
+    $title = $mp3->getTitle($path); // Получить название трека
+    $trackNo = $mp3->getTrackNo($path); // Получить номер трека
+    $bitrate = $mp3->getBitrate($path); // Получить битрейт
+    $duration = $mp3->getDuration($path); // Получить длительность трека
+    $format = $mp3->getFormat($path); // Получить формат трека
+    $mime = $mp3->getMime($path); // Получить MIME тип файла
+    $isLossless = $mp3->isLossless($path); // Ялвяется ли lossless
+
+    dd($artist, $album, $year, $genre, $title, $trackNo, $bitrate, $duration, $format, $mime, $isLossless);
 
 })->name('test2');
 
 Route::get('/test3', function() {
-    $filters = [
-        'tag' => 'industrial metal',
-    ];
-    $result = (new Artist)->getFiltered($filters);
-    dd($result);
-})->name('test2');
+    $filters = [];
+//    $resource = Artist::filter($filters, 'tag', 'tags', 'name')
+//                      ->when(array_key_exists('offset', $filters), function ($q) use ($filters) {
+//                          $q->offset($filters['offset'])->limit($filters['limit']);
+//                      })
+//                      ->cursorPaginate(5)->links();
+//    dd($resource);
+    return new ArtistCollection(Artist::cursorPaginate(5));
+})->name('test3');
