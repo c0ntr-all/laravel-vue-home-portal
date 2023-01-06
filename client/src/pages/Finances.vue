ede0fe8643331bfa60933a0814e3f8b0568709ad
c0ntr-all <serg.taturin@gmail.com>
2023-01-06 22:39:16 +0300

    Finances: Rendering finances from API

--- client/src/pages/Finances.vue
+++ client/src/pages/Finances.vue
@@ -1,9 +1,21 @@
 <template>
   <q-page padding>
     <div class="column q-gutter-md">
-      <q-card v-for="number in [1,2,3,4,5]" class="finances-card" flat bordered>
-        <q-card-section horizontal>
-          <q-card-section>test {{ number }}</q-card-section>
+      <q-card v-for="item in finances" class="finances-card" flat bordered>
+        <q-card-section horizontal class="flex justify-between">
+          <q-card-section class="flex column justify-between">
+            <p class="text-grey-5">{{ item.doneAt }}</p>
+            <p>{{ item.comment }}</p>
+            <p
+              class="text-subtitle2 text-weight-bolder q-mb-none"
+              :class="{
+                'text-red-6': item.type === 'outcome',
+                'text-green-6': item.type === 'income'
+              }"
+            >
+              {{ item.summ }}
+            </p>
+          </q-card-section>
           <q-card-actions vertical class="justify-around q-px-md">
             <q-btn flat round color="red" icon="favorite" />
             <q-btn flat round color="accent" icon="bookmark" />
@@ -24,7 +36,8 @@ export default {
   setup() {
     const finances = ref([])
     const getFinances = async () => {
-      finances.value = await API.get('finances')
+      const {data} = await API.get('finances')
+      finances.value = data.finances
     }
 
     return {
