1a46fcbe93240c50753c20ad7dfe23f499610a2d
c0ntr-all <serg.taturin@gmail.com>
2023-01-21 21:15:15 +0300

    Laravel: Added cors middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
-                ->middleware('api')
+                ->middleware(['api', 'cors'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
