fa93db75151e7c6819aeee5e6fdf139b0d6a97cf
c0ntr-all <serg.taturin@gmail.com>
2023-01-24 23:25:31 +0300

    MusicPlayer: Styling player

--- client/src/components/default/MusicPlayer.vue
+++ client/src/components/default/MusicPlayer.vue
@@ -1,31 +1,38 @@
 <template>
   <q-btn label="Open Player" color="primary" @click="dialog = true" />
   <q-dialog v-model="dialog" position="right">
-    <q-card style="width: 350px">
-      <q-linear-progress :value="0.6" color="pink" />
-
+    <q-card style="width: 352px">
       <q-card-section class="row items-center no-wrap">
-        <div>
-          <div class="text-weight-bold">The Walker</div>
-          <div class="text-grey">Fitz & The Tantrums</div>
-        </div>
+        <q-img
+          :src="'https://placeimg.com/500/300/nature?t=' + Math.random()"
+          spinner-color="white"
+          style="width:320px; height: 320px"
+        />
+      </q-card-section>
 
-        <q-space />
+      <q-card-section class="row justify-center no-wrap q-pa-sm">
+        <span class="text-weight-bold">The Walker</span>. <span class="text-grey">Fitz & The Tantrums</span>
+      </q-card-section>
 
+      <q-card-section class="row justify-center no-wrap q-pa-sm">
         <q-btn flat round icon="fast_rewind" />
         <q-btn flat round icon="pause" />
         <q-btn flat round icon="fast_forward" />
+      </q-card-section>
+
+      <q-card-section class="row items-center no-wrap q-px-md q-pt-sm">
+        <q-linear-progress :value="0.6" color="pink" />
       </q-card-section>
     </q-card>
   </q-dialog>
 </template>
 <script>
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 export default {
   setup() {
     return {
-      dialog: ref(false)
+      dialog: ref(false),
     }
   }
 }
