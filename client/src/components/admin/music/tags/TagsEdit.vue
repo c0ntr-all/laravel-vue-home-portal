09372051cdd2c6505ec0c0c9b84f0468ed35597d
c0ntr-all <serg.taturin@gmail.com>
2023-01-31 02:19:30 +0300

    Admin: Added methods to tags CRUD

--- client/src/components/admin/music/tags/TagsEdit.vue
+++ client/src/components/admin/music/tags/TagsEdit.vue
@@ -19,29 +19,49 @@
             {{ prop.node.label }}
           </div>
           <div class="tag__actions q-gutter-x-xs">
-            <q-btn size="xs" color="primary" dense>
+            <q-btn @click="addTagDialog = true" size="xs" color="primary" dense>
               <q-icon name="add" size="15px"></q-icon>
             </q-btn>
-            <q-btn size="xs" color="secondary" dense>
+            <q-btn @click="editTagDialog = true" size="xs" color="secondary" dense>
               <q-icon name="edit" size="15px"></q-icon>
             </q-btn>
-            <q-btn size="xs" color="red" dense>
+            <q-btn @click="deleteTagDialog = true" size="xs" color="red" dense>
               <q-icon name="delete" size="15px"></q-icon>
             </q-btn>
           </div>
         </div>
       </template>
     </q-tree>
+    <q-dialog v-model="addTagDialog">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Close icon</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 <script>
  import { ref, onMounted } from 'vue'
- import API from "src/utils/api";
+ import { useQuasar } from "quasar"
+ import API from "src/utils/api"
 
  export default {
    setup() {
+     const $q = useQuasar()
+
      const tags = ref([])
-     const selectedTag = ref('')
+     const filter = ref('')
+     const addTagDialog = ref(false)
+     const editTagDialog = ref(false)
+     const deleteTagDialog = ref(false)
+
      const getTags = async () => {
        await API.post('music/tags/tree')
          .then(response => {
@@ -50,13 +70,50 @@
            return false
          })
      }
+     const addTag = async () => {
+       await API.post('music/tags/store', tag)
+         .then(response => {
+           $q.notify({
+             type: 'positive',
+             message: `Тег ${response.label} успешно добавлен!`
+           })
+       }).catch(error => {
+           $q.notify({
+             type: 'negative',
+             message: error
+           })
+       })
+     }
+     const editTag = async () => {
+       await API.post('music/tags/update', tag)
+         .then(response => {
+           $q.notify({
+             type: 'positive',
+             message: `Тег ${response.label} успешно обновлён!`
+           })
+       }).catch(error => {
+           $q.notify({
+             type: 'negative',
+             message: error
+           })
+       })
+     }
+     const deleteTag = async () => {
+
+     }
      onMounted(() => {
        getTags()
      })
      return {
        tags,
-       selectedTag,
-       getTags
+       filter,
+       addTagDialog,
+       editTagDialog,
+       deleteTagDialog,
+       getTags,
+       addTag,
+       editTag,
+       deleteTag
      }
    }
  }
