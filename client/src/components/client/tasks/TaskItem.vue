17d5175bebabdfc68ec8091add59145721830913
c0ntr-all <serg.taturin@gmail.com>
2023-03-05 01:27:48 +0300

    Tasks: Added content editing and refactored title editing

--- client/src/components/client/tasks/TaskItem.vue
+++ client/src/components/client/tasks/TaskItem.vue
@@ -12,8 +12,23 @@
       <q-card-section class="task__header">
         <div class="task__title text-h6">
           {{ item.title }}
-          <q-popup-edit v-model="item.title" auto-save v-slot="scope">
-            <q-input v-model="scope.value" dense autofocus counter @keyup.enter="scope.set" />
+          <q-popup-edit
+            ref="titlePopup"
+            v-model="item.title"
+            auto-save
+            v-slot="scope"
+          >
+            <q-input
+              v-model="scope.value"
+              @keyup.enter="saveTitle(scope.value)"
+              dense
+              autofocus
+              counter
+            />
+            <div class="q-pt-sm">
+              <q-btn @click="saveTitle(scope.value)" label="Сохранить" color="primary" flat />
+              <q-btn @click="titlePopup.cancel()" label="Отмена" color="primary" flat />
+            </div>
           </q-popup-edit>
         </div>
         <q-btn @click="showModal = false" class="task__close" icon="close" size="md" flat rounded />
@@ -22,8 +37,24 @@
       <q-separator dark />
 
       <q-card-section>
-        <div v-if="item.content" class="task__content">{{ item.content }}</div>
-        <p v-else class="text-grey-5">Описание отсутствует!</p>
+        <div v-if="item.content" v-html="item.content" class="task__content"></div>
+        <div v-else class="task__content text-grey-5">Описание отсутствует!</div>
+        <q-popup-edit
+          ref="contentPopup"
+          v-model="item.content"
+          v-slot="scope"
+        >
+          <q-editor
+            v-model="scope.value"
+            min-height="5rem"
+            autofocus
+            @keyup.enter.stop
+          />
+          <div class="q-pt-sm">
+            <q-btn @click="saveContent(scope.value)" label="Сохранить" color="primary" flat />
+            <q-btn @click="contentPopup.cancel()" label="Отмена" color="primary" flat />
+          </div>
+        </q-popup-edit>
       </q-card-section>
 
       <q-card-section>
@@ -47,7 +78,7 @@
   </q-dialog>
 </template>
 <script>
-import {ref, watch} from 'vue'
+import {ref} from 'vue'
 import {useQuasar} from "quasar"
 
 import API from "src/utils/api"
@@ -58,25 +89,49 @@ export default {
     const $q = useQuasar()
 
     const showModal = ref(false)
-    watch(props.item, (newValue, oldValue) => {
-      API.post(`tasks/${props.item.id}/update`, {
-        title: props.item.title
+    const titlePopup = ref(null)
+    const contentPopup = ref(null)
+    const saveTitle = value => {
+      API.patch(`tasks/${props.item.id}/update`, {
+        title: value
       }).then(response => {
         $q.notify({
           type: 'positive',
           message: 'Имя карточки успешно обновлено!'
         })
+        titlePopup.value.set()
       }).catch(error => {
-        // todo now the old value is the same as the new. Need to catch old value and assign it to props if error fired
-        props.item.title = oldValue.title
         $q.notify({
           type: 'negative',
           message: `Server Error: ${error.response.data.message}`
         })
+        titlePopup.value.cancel()
       })
-    })
+    }
+    const saveContent = value => {
+      API.patch(`tasks/${props.item.id}/update`, {
+        content: value
+      }).then(response => {
+        $q.notify({
+          type: 'positive',
+          message: 'Описание успешно обновлено!'
+        })
+        contentPopup.value.set()
+      }).catch(error => {
+        $q.notify({
+          type: 'negative',
+          message: `Server Error: ${error.response.data.message}`
+        })
+        contentPopup.value.cancel()
+      })
+    }
+
     return {
-      showModal
+      showModal,
+      titlePopup,
+      contentPopup,
+      saveTitle,
+      saveContent
     }
   }
 }
@@ -87,6 +142,20 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: center;
+  }
+  &__title {
+    width: 100%;
+
+    &:hover {
+      cursor: pointer;
+      background: #f4f5f7;
+    }
+  }
+  &__content {
+    &:hover {
+      cursor: pointer;
+      background: #f4f5f7;
+    }
   }
   &-item {
     word-break: break-all;
