87a71d7a1d24eb0c6d906e15879f5275c6f082aa
c0ntr-all <serg.taturin@gmail.com>
2023-03-04 03:46:27 +0300

    Tasks: Added title editing to tasks

--- client/src/components/client/tasks/TaskItem.vue
+++ client/src/components/client/tasks/TaskItem.vue
@@ -9,20 +9,27 @@
 
   <q-dialog v-model="showModal">
     <q-card style="width: 768px; max-width: 80vw;">
-      <q-card-section>
-        <div class="task__title text-h6">{{ item.title }}</div>
+      <q-card-section class="task__header">
+        <div class="task__title text-h6">
+          {{ item.title }}
+          <q-popup-edit v-model="item.title" auto-save v-slot="scope">
+            <q-input v-model="scope.value" dense autofocus counter @keyup.enter="scope.set" />
+          </q-popup-edit>
+        </div>
+        <q-btn @click="showModal = false" class="task__close" icon="close" size="md" flat rounded />
       </q-card-section>
 
       <q-separator dark />
 
       <q-card-section>
-        <div class="task__content">{{ item.content }}</div>
+        <div v-if="item.content" class="task__content">{{ item.content }}</div>
+        <p v-else class="text-grey-5">Описание отсутствует!</p>
       </q-card-section>
 
-      <q-card-section v-if="item.comments.length">
+      <q-card-section>
         <div class="task__comments">
           <div class="text-h6 q-mb-sm">Комментарии</div>
-          <div class="comments-list column q-gutter-sm">
+          <div v-if="item.comments.length" class="comments-list column q-gutter-sm">
             <q-card v-for="comment in item.comments" :key="comment.id">
               <q-card-section class="flex justify-between">
                 <span>{{ comment.user_name }}</span>
@@ -33,17 +40,41 @@
               </q-card-section>
             </q-card>
           </div>
+          <p v-else class="text-grey-5">Комментарии отсутствуют!</p>
         </div>
       </q-card-section>
     </q-card>
   </q-dialog>
 </template>
 <script>
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
+import {useQuasar} from "quasar"
+
+import API from "src/utils/api"
+
 export default {
   props: ['item'],
-  setup() {
+  setup(props) {
+    const $q = useQuasar()
+
     const showModal = ref(false)
+    watch(props.item, (newValue, oldValue) => {
+      API.post(`tasks/${props.item.id}/update`, {
+        title: props.item.title
+      }).then(response => {
+        $q.notify({
+          type: 'positive',
+          message: 'Имя карточки успешно обновлено!'
+        })
+      }).catch(error => {
+        // todo now the old value is the same as the new. Need to catch old value and assign it to props if error fired
+        props.item.title = oldValue.title
+        $q.notify({
+          type: 'negative',
+          message: `Server Error: ${error.response.data.message}`
+        })
+      })
+    })
     return {
       showModal
     }
@@ -51,31 +82,33 @@ export default {
 }
 </script>
 <style lang="scss">
-.card-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.task {
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &-item {
+    word-break: break-all;
+    margin-bottom: 8px;
+
+    &__link {
+      display: block;
+      padding: 8px;
+      background-color: #fff;
+      text-decoration: none;
+      color: #000;
+      border-radius: 3px;
+      box-shadow: 0 1px 0 #091e4240;
+
+      &:hover {
+        background-color: #f4f5f7;
+        border-bottom-color: #091e4240;
+      }
+    }
+  }
 }
 .text {
   font-size: 14px;
-}
-.task-item {
-  word-break: break-all;
-  margin-bottom: 8px;
-
-  &__link {
-    display: block;
-    padding: 8px;
-    background-color: #fff;
-    text-decoration: none;
-    color: #000;
-    border-radius: 3px;
-    box-shadow: 0 1px 0 #091e4240;
-
-    &:hover {
-      background-color: #f4f5f7;
-      border-bottom-color: #091e4240;
-    }
-  }
 }
 </style>
