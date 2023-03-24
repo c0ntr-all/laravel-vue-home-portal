9098bd2b86c3bf7c948cbfe5fabae7d8bde330ed
c0ntr-all <serg.taturin@gmail.com>
2023-03-24 20:47:00 +0300

    Reminds: Added skeleton to Reminds

---
+++ client/src/components/client/reminds/skeleton/RemindsPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="q-mb-lg">
+    <q-skeleton type="QBtn" width="180px" />
+  </div>
+  <q-markup-table>
+    <thead>
+    <tr>
+      <th class="text-left">
+        <q-skeleton type="text" width="15px" />
+      </th>
+      <th class="text-right">
+        <q-skeleton type="text" width="24px" />
+      </th>
+      <th class="text-right">
+        <q-skeleton type="text" width="80px" />
+      </th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr v-for="n in 5" :key="n">
+      <td class="text-left">
+        <q-skeleton type="text" width="15px" />
+      </td>
+      <td class="text-right">
+        <q-skeleton type="text" width="500px" />
+      </td>
+      <td class="text-right">
+        <q-skeleton type="text" width="65px" />
+      </td>
+    </tr>
+    </tbody>
+  </q-markup-table>
+</template>
+<script>
+
+</script>
+<style>
+
+</style>
--- client/src/pages/client/Reminds.vue
+++ client/src/pages/client/Reminds.vue
@@ -1,107 +1,111 @@
 <template>
-  <div class="q-mb-lg">
-    <q-btn @click="showModal = true" color="primary" label="Create Remind" icon="add" />
-  </div>
-  <q-table
-    :rows="reminds"
-    :columns="columns"
-    row-key="title"
-    :flat="true"
-    :rows-per-page-options="[0]"
-    :pagination.sync="{page: 1, rowsPerPage: 0}"
-    ref="remindTable"
-    dense
-  >
-    <template v-slot:header="props">
-      <q-tr :props="props">
-        <q-th auto-width />
-        <q-th
-          v-for="col in props.cols"
-          :key="col.name"
-          :props="props"
-        >
-          {{ col.label }}
-        </q-th>
-      </q-tr>
-    </template>
+  <RemindsTableSkeleton v-if="loading" />
 
-    <template v-slot:body="props">
-      <q-tr :props="props">
-        <q-td auto-width>
-          <q-btn size="sm" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'expand_more' : 'chevron_right'" />
-        </q-td>
-        <q-td
-          v-for="col in props.cols"
-          :key="col.name"
-          :props="props"
-        >
-          <table-col :col="col" :row="props.row" @activeSwitched="sortReminds" />
-        </q-td>
-      </q-tr>
-      <q-tr v-show="props.expand" :props="props">
-        <q-td colspan="100%">
-          <div class="text-left">This is expand slot for row above: {{ props.row.title }}.</div>
-        </q-td>
-      </q-tr>
-    </template>
-  </q-table>
-  <q-dialog v-model="showModal">
-    <q-card style="width: 700px; max-width: 80vw;">
-      <q-card-section>
-        <div class="text-h6">Добавить напоминание</div>
-      </q-card-section>
+  <template v-else>
+    <div class="q-mb-lg">
+      <q-btn @click="showModal = true" color="primary" label="Create Remind" icon="add" />
+    </div>
+    <q-table
+      :rows="reminds"
+      :columns="columns"
+      row-key="title"
+      :flat="true"
+      :rows-per-page-options="[0]"
+      :pagination.sync="{page: 1, rowsPerPage: 0}"
+      ref="remindTable"
+      dense
+    >
+      <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th auto-width />
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
 
-      <q-card-section class="q-pt-none">
-        <q-form class="q-gutter-y-md column">
-          <q-input
-            v-model="model.title"
-            label="Заголовок"
-            :rules="[ val => val && val.length > 0 || 'Поле title должно быть заполнено!']"
-            outlined
-            dense
-          />
-          <q-input
-            v-model="model.content"
-            label="Описание"
-            type="textarea"
-            outlined
-            dense
-          />
-          <q-input v-model="model.datetime" outlined filled>
-            <template v-slot:prepend>
-              <q-icon name="event" class="cursor-pointer">
-                <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                  <q-date v-model="model.datetime" mask="YYYY-MM-DD HH:mm">
-                    <div class="row items-center justify-end">
-                      <q-btn label="Close" color="primary" v-close-popup flat />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td auto-width>
+            <q-btn size="sm" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'expand_more' : 'chevron_right'" />
+          </q-td>
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            <table-col :col="col" :row="props.row" @activeSwitched="sortReminds" />
+          </q-td>
+        </q-tr>
+        <q-tr v-show="props.expand" :props="props">
+          <q-td colspan="100%">
+            <div class="text-left">This is expand slot for row above: {{ props.row.title }}.</div>
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
+    <q-dialog v-model="showModal">
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section>
+          <div class="text-h6">Добавить напоминание</div>
+        </q-card-section>
 
-            <template v-slot:append>
-              <q-icon name="access_time" class="cursor-pointer">
-                <q-popup-proxy transition-show="scale" transition-hide="scale" cover>
-                  <q-time v-model="model.datetime" mask="YYYY-MM-DD HH:mm" format24h>
-                    <div class="row items-center justify-end">
-                      <q-btn label="Close" color="primary" v-close-popup flat />
-                    </div>
-                  </q-time>
-                </q-popup-proxy>
-              </q-icon>
-            </template>
-          </q-input>
-        </q-form>
-      </q-card-section>
+        <q-card-section class="q-pt-none">
+          <q-form class="q-gutter-y-md column">
+            <q-input
+              v-model="model.title"
+              label="Заголовок"
+              :rules="[ val => val && val.length > 0 || 'Поле title должно быть заполнено!']"
+              outlined
+              dense
+            />
+            <q-input
+              v-model="model.content"
+              label="Описание"
+              type="textarea"
+              outlined
+              dense
+            />
+            <q-input v-model="model.datetime" outlined filled>
+              <template v-slot:prepend>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="model.datetime" mask="YYYY-MM-DD HH:mm">
+                      <div class="row items-center justify-end">
+                        <q-btn label="Close" color="primary" v-close-popup flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
 
-      <q-card-actions align="right" class="bg-white">
-        <q-btn label="Create" color="primary" @click="createRemind" :loading="createRemindLoading" />
-        <!--Todo: Need to write cancel update handler for returning previous values to model-->
-        <q-btn label="Cancel" v-close-popup />
-      </q-card-actions>
-    </q-card>
-  </q-dialog>
+              <template v-slot:append>
+                <q-icon name="access_time" class="cursor-pointer">
+                  <q-popup-proxy transition-show="scale" transition-hide="scale" cover>
+                    <q-time v-model="model.datetime" mask="YYYY-MM-DD HH:mm" format24h>
+                      <div class="row items-center justify-end">
+                        <q-btn label="Close" color="primary" v-close-popup flat />
+                      </div>
+                    </q-time>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </q-form>
+        </q-card-section>
+
+        <q-card-actions align="right" class="bg-white">
+          <q-btn label="Create" color="primary" @click="createRemind" :loading="createRemindLoading" />
+          <!--Todo: Need to write cancel update handler for returning previous values to model-->
+          <q-btn label="Cancel" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </template>
 </template>
 <script>
 import {ref, onMounted} from 'vue'
@@ -109,15 +113,18 @@ import {useQuasar} from "quasar"
 
 import API from "src/utils/api"
 
+import RemindsTableSkeleton from 'src/components/client/reminds/skeleton/RemindsPage.vue'
 import TableCol from 'src/components/client/reminds/RemindsTableCol.vue'
 
 export default {
   components: {
+    RemindsTableSkeleton,
     TableCol
   },
   setup() {
     const $q = useQuasar()
 
+    let loading = ref(true)
     const remindTable = ref(null)
     const columns = [
       { name: 'title', label: 'Title', field: 'title', align: 'left', sortable: true },
@@ -138,11 +145,13 @@ export default {
     const getReminds = async () => {
       await API.get('reminds').then(response => {
         reminds.value = response.data.reminds
+        loading.value = false
       }).catch(error => {
         $q.notify({
           type: 'negative',
           message: `Server Error: ${error.response.data.message}`
         })
+        loading.value = false
       })
     }
     const createRemind = async () => {
@@ -177,6 +186,7 @@ export default {
     })
 
     return {
+      loading,
       columns,
       reminds,
       remindTable,
