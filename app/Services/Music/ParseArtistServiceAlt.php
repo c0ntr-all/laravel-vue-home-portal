05bb413d80e77127e2fdaa6ade300065f82c0c59
c0ntr-all <serg.taturin@gmail.com>
2023-04-29 23:54:00 +0300

    Music: Alternative music parser. Based on Track info of artist, album and other. Raw version

--- app/Services/Music/ParseArtistServiceAlt.php
+++ app/Services/Music/ParseArtistServiceAlt.php
@@ -3,19 +3,14 @@
 namespace App\Services\Music;
 
 use App\Models\Music\Artist;
-use App\Models\Music\Album;
-use App\Models\Music\Track;
 use App\Helpers\ImageUpload;
 use getID3;
-use Illuminate\Http\File;
 
 class ParseArtistServiceAlt
 {
     private string $noImage = 'no-image.gif';
 
     private const EXTENSIONS = ['mp3'];
-
-    private const TYPES = ['lp', 'ep', 'single', 'demo', 'split', 'tribute', 'bootleg', 'live', 'instrumental', 'remaster'];
 
     public function __construct(private getID3 $getID3)
     {
@@ -35,6 +30,61 @@ class ParseArtistServiceAlt
 
     private function parseFolder(string $folder)
     {
+        $albums = $this->findAlbums($folder);
+
+        if (!empty($albums)) {
+            foreach ($albums as $albumPath) {
+                $albumItems = scandir($albumPath);
+
+                $cover = $this->getCover($albumPath);
+
+                foreach ($albumItems as $key => $item) {
+                    if ($item === '.' || $item === '..') {
+                        continue;
+                    }
+
+                    $info = pathinfo($item);
+
+                    if (isset($info['extension']) && $info['extension'] == 'mp3') {
+                        $trackPath = $albumPath . DIRECTORY_SEPARATOR . $item;
+                        $id3TrackInfo = $this->getID3->analyze($trackPath);
+
+                        $artistName = $id3TrackInfo['id3v2']['comments']['artist'][0];
+
+                        // Добавляем исполнителя по имени
+                        $artist = Artist::firstOrCreate([
+                            'name' => $artistName
+                        ],[
+                            'image' => $cover,
+                            'path' => $folder
+                        ]);
+
+                        $albumName = $id3TrackInfo['id3v2']['comments']['album'][0];
+
+                        $album = $artist->albums()->firstOrCreate([
+                            'name' => $albumName
+                        ],[
+                            'image' => $cover,
+                            'year' => $id3TrackInfo['id3v2']['comments']['year'][0],
+                            'path' => $albumPath
+                        ]);
+
+                        $trackName = $id3TrackInfo['id3v2']['comments']['title'][0];
+
+                        $album->tracks()->updateOrCreate([
+                            'album_id' => $album->id,
+                            'name' => $trackName
+                        ],[
+                            'number' => $id3TrackInfo['id3v2']['comments']['track_number'][0] ?? NULL,
+                            'duration' => $id3TrackInfo['playtime_string'],
+                            'bitrate' => $id3TrackInfo['audio']['bitrate'],
+                            'path' => $trackPath
+                        ]);
+                    }
+                }
+            }
+        }
+
         $dirCanonical = realpath($folder);
 
         static $items = [];
@@ -50,33 +100,6 @@ class ParseArtistServiceAlt
 
                 if (is_dir($dirItem)) {
                     $this->parseFolder($dirItem);
-                } else {
-                    if ($info['extension'] === 'mp3') {
-                        $albumDir = $this->getAlbumDir($dirItem);
-
-                        $coverOriginalPath = $albumDir . DIRECTORY_SEPARATOR . 'Cover.jpg';
-
-                        if (file_exists($coverOriginalPath)) {
-
-//                            $imageObject = ImageUpload::make()
-//                                                      ->setDiskName('public')
-//                                                      ->setFolder('music/albums/posters')
-//                                                      ->setSourceName(basename($albumDir));
-
-//                            if ($imageObject->exists('jpg')) {
-//
-//                            }
-
-                            $items[] = [
-                                'track' => $dirItem,
-                                'cover' => ImageUpload::make()
-                                                      ->setDiskName('public')
-                                                      ->setFolder('music/albums/posters')
-                                                      ->setSourceName(basename($albumDir))
-                                                      ->getFilePath('jpg')
-                            ];
-                        }
-                    }
                 }
             }
 
@@ -101,5 +124,51 @@ class ParseArtistServiceAlt
                 return null;
             }
         }
+    }
+
+    public function findAlbums(string $path): array
+    {
+        $dirCanonical = realpath($path);
+
+        static $items = [];
+
+        if ($dirStream = opendir($dirCanonical)) {
+            while (false !== ($fileName = readdir($dirStream))) {
+                if ($fileName == "." || $fileName == "..") {
+                    continue;
+                }
+
+                $dirItem = $dirCanonical . DIRECTORY_SEPARATOR . $fileName;
+
+                if (preg_match('/[0-9]{4} - .*/i', basename($fileName))) {
+                    $items[] = $dirItem;
+                } else if(is_dir($dirItem)) {
+                    $this->findAlbums($dirItem);
+                }
+            }
+        }
+
+        return $items;
+    }
+
+    private function getCover(string $albumPath): string|null
+    {
+        $defaultCover = $albumPath . DIRECTORY_SEPARATOR . 'Cover.jpg';
+
+        if (file_exists($defaultCover)) {
+            return $defaultCover;
+        } else {
+            $albumItems = scandir($albumPath);
+
+            foreach ($albumItems as $item) {
+                $info = pathinfo($item);
+
+                if (in_array($info['extension'], ['jpg', 'png', 'jpeg'])) {
+                    return $item;
+                }
+            }
+        }
+
+        return null;
     }
 }
