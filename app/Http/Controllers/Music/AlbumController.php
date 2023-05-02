0aaf45b79164eac171d573de0e1a6ed52ca76018
c0ntr-all <serg.taturin@gmail.com>
2023-05-02 22:21:00 +0300

    Music: Added redirect for no album

--- app/Http/Controllers/Music/AlbumController.php
+++ app/Http/Controllers/Music/AlbumController.php
@@ -6,19 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Music\Album\IndexRequest;
 use App\Models\Music\Album;
 use App\Http\Resources\Music\AlbumResource;
+use Illuminate\Http\Response;
 
 class AlbumController extends Controller
 {
 
-    public function index(IndexRequest $request): array
+    public function index(IndexRequest $request): array|Response
     {
         return $this->albumResponse(Album::find($request->validated()['id']));
     }
 
-    public function albumResponse(Album $album): array
+    public function albumResponse($album): array|Response
     {
-        $resource = new AlbumResource($album);
+        if ($album) {
+            $resource = new AlbumResource($album);
 
-        return ['success' => true, 'data' => $resource];
+            return ['success' => true, 'data' => $resource];
+        } else {
+            return response(['success' => false, 'message' => 'Album not Found!'], 404);
+        }
     }
 }
