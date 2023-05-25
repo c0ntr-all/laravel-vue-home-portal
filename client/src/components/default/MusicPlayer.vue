98a4d2f7b965352353b75c145cf4beb077c60f5a
c0ntr-all <serg.taturin@gmail.com>
2023-05-25 23:18:00 +0300

    Update: Changed volume slider to app slider

--- client/src/components/default/MusicPlayer.vue
+++ client/src/components/default/MusicPlayer.vue
@@ -69,26 +69,16 @@
                 {{ musicPlayer.timePassed }}
               </div>
             </div>
-            <div class="music-player-slider col-grow">
-              <div class="music-player-slider__wrapper" ref="rangeLine">
-                <div class="music-player-slider__line">
-                  <div class="music-player-slider__amount" :style="`width: ${musicPlayer.rewindProgressWidth}%`"></div>
-                  <div class="music-player-slider__circle" :style="`left: ${circlePosition}%`" ref="rangeHandle"></div>
-                </div>
-              </div>
-            </div>
+
+            <AppSlider />
+
           </div>
         </div>
+
         <div class="music-player__volume flex items-center q-mx-md">
-          <div class="music-player-slider col-grow">
-            <div class="music-player-slider__wrapper" ref="rangeVolume">
-              <div class="music-player-slider__line">
-                <div class="music-player-slider__amount" :style="`width: ${musicPlayer.volume * 100}%`"></div>
-                <div class="music-player-slider__circle" :style="`left: ${musicPlayer.volume * 100}%`"></div>
-              </div>
-            </div>
-          </div>
+          <AppSlider :width="'50px'" />
         </div>
+
         <div class="music-player__buttons-group flex items-center">
           <q-btn
             @click="musicPlayer.shuffle()"
@@ -117,108 +107,16 @@
   </div>
 </template>
 <script>
-import {ref} from 'vue'
-import {useMusicPlayer} from "src/stores/modules/musicPlayer";
+import {ref} from "vue"
+import {useMusicPlayer} from "src/stores/modules/musicPlayer"
 
-import MusicTrackCard from "src/components/client/music/MusicTrackCard.vue";
+import AppSlider from "src/components/extra/AppSlider.vue"
+import MusicTrackCard from "src/components/client/music/MusicTrackCard.vue"
 
 export default {
-  components: { MusicTrackCard },
+  components: { AppSlider, MusicTrackCard },
   setup() {
     const musicPlayer = useMusicPlayer()
-
-    const rangeLine = ref(null)
-    const rangeHandle = ref(null)
-
-    const rangeVolume = ref(null)
-    const isVolumeCircleMoving = ref(false)
-    let circlePosition = ref(null)
-    const isCircleMoving = ref(false)
-
-    const min = 0
-    const max = 100
-
-    let newValue = ref(null)
-
-    const _move = clientX => {
-      let bounds = rangeLine.value.getBoundingClientRect()
-      let posX = (clientX - bounds.left) / bounds.width
-
-      let value = min + (max - min) * posX
-      value = Math.round(Math.min(Math.max(value, min), max))
-
-      if (value < min || value > max) {
-        return
-      }
-
-      circlePosition.value = value
-
-      if (newValue.value == null || (value !== newValue.value)) {
-        newValue.value = value
-      }
-    }
-
-    const process = (clientX, target) => {
-      if (target !== rangeHandle.value) {
-        _move(clientX)
-      }
-
-      let move = (e) => {
-        e.preventDefault()
-        _move(e.clientX)
-      };
-
-      let end = () => {
-        document.removeEventListener("mousemove", move)
-        document.removeEventListener("touchmove", move)
-        document.removeEventListener("mouseup", end)
-        document.removeEventListener("touchend", end)
-        document.removeEventListener("touchcancel", end)
-      };
-
-      document.addEventListener("mousemove", move)
-      document.addEventListener("touchmove", move)
-      document.addEventListener("mouseup", end)
-      document.addEventListener("touchend", end)
-      document.addEventListener("touchcancel", end)
-    }
-
-    const initPlayerParams = () => {
-      rangeLine.value.addEventListener("mousedown", event => {
-        // Only left mouse button
-        if (event.button !== 0) {
-          return
-        }
-        event.preventDefault();
-
-        process(event.clientX, event.target)
-      });
-
-      rangeLine.value.addEventListener("touchstart", event => {
-        if (event.changedTouches && event.changedTouches[0]) {
-          event.preventDefault();
-          process(event.changedTouches[0].clientX, event.target);
-        }
-      });
-
-      rangeVolume.value.addEventListener('mousedown', () => {
-        isVolumeCircleMoving.value = true
-      })
-      rangeVolume.value.addEventListener('mousemove', event => {
-        if (isVolumeCircleMoving.value) {
-          musicPlayer.audio.volume = (event.offsetX / rangeVolume.value.clientWidth)
-        }
-      })
-      rangeVolume.value.addEventListener('click', event => {
-        musicPlayer.audio.volume = (event.offsetX / rangeVolume.value.clientWidth)
-      })
-      rangeVolume.value.addEventListener('mouseup', () => {
-        isVolumeCircleMoving.value = false
-      })
-      rangeVolume.value.addEventListener('mouseleave', () => {
-        isVolumeCircleMoving.value = false
-      })
-    }
 
     musicPlayer.init()
 
@@ -226,13 +124,6 @@ export default {
       dialog: ref(false),
       hovered: ref(false),
       musicPlayer,
-      rangeLine,
-      rangeHandle,
-      rangeVolume,
-      circlePosition,
-      isCircleMoving,
-      isVolumeCircleMoving,
-      initPlayerParams,
       initPlay: track => {
         musicPlayer.playTrack(track)
       },
@@ -270,50 +161,6 @@ export default {
     font-size: 12.5px;
     line-height: 16px;
     font-weight: bold;
-  }
-  &__volume {
-    width: 50px;
-  }
-  &-slider {
-    &__wrapper {
-      padding: 7px 0;
-
-      &:hover {
-        .music-player-slider__circle {
-          opacity: 1;
-        }
-      }
-    }
-    &__line {
-      position: relative;
-      width: 100%;
-      height: 2px;
-      background: $primary-light;
-      z-index: -1;
-    }
-    &__amount {
-      width: 0;
-      top: auto;
-      bottom: 0;
-      height: 2px;
-      background: $primary;
-    }
-    &__circle {
-      position: absolute;
-      top: -2px;
-      width: 6px;
-      height: 6px;
-      margin-left: -3px;
-      border-radius: 50%;
-      background: $primary;
-      opacity: 0;
-      -webkit-transition: top 80ms linear,width 80ms linear,height 80ms linear,margin-left 80ms linear,opacity 160ms linear;
-      transition: top 80ms linear,width 80ms linear,height 80ms linear,margin-left 80ms linear,opacity 160ms linear;
-    }
-
-    &:hover {
-      cursor: pointer;
-    }
   }
 }
 </style>
