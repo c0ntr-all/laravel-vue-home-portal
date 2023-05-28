49a3fdcd2a29ce4127c7f3cad71c44dc6a9098ce
c0ntr-all <serg.taturin@gmail.com>
2023-05-28 19:46:00 +0300

    Feature: Added basic timer for counting down tracks

---
+++ client/src/utils/timer.js
@@ -0,0 +1,72 @@
+export default class Timer {
+  constructor() {
+    this.targetSeconds = null;
+    this.startedAt = null;
+    this.pausedAt = null;
+    this.pausedSeconds = 0;
+    this.timerId = null
+
+    this.clearTrigger();
+  }
+
+  start() {
+    this.clearTrigger();
+    this.startedAt = this.now();
+  }
+
+  pause() {
+    this.pausedAt = this.now();
+    this.clearTrigger();
+  }
+
+  resume() {
+    this.pausedSeconds += this.now() - this.pausedAt;
+    this.pausedAt = null;
+
+    if (!this.timerId && this.targetSeconds !== null) {
+      this.setTrigger(this.targetSeconds - this.getElapsed());
+    }
+  }
+
+  update(seconds) {
+    this.targetSeconds = seconds;
+
+    if (this.targetSeconds !== null) {
+      if (this.pausedAt === null) {
+        this.setTrigger(this.targetSeconds - this.getElapsed());
+      }
+    } else {
+      this.clearTrigger();
+    }
+  }
+
+  getRemainingSeconds() {
+    return this.targetSeconds !== null ? this.targetSeconds - this.getElapsed() : null;
+  }
+
+  setTrigger(seconds) {
+    this.clearTrigger();
+    this.timeoutId = setTimeout(seconds * 1000);
+  }
+
+  clearTrigger() {
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+    }
+    this.timerId = null;
+  }
+
+  getElapsed() {
+    let elapsed = this.now() - this.startedAt - this.pausedSeconds;
+
+    if (this.pausedAt !== null) {
+      elapsed -= this.now() - this.pausedAt;
+    }
+
+    return elapsed;
+  }
+
+  now() {
+    return Math.round(new Date().valueOf() / 1000);
+  }
+}
